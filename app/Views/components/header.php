ab2d003130a3a6609c2aac8744b388127ef200d1
unknown <danilim430@gmail.com>
2025-06-10 08:09:39 +0700

    edit file readme

--- app/Views/components/header.php
+++ app/Views/components/header.php
@@ -175,7 +175,7 @@
 
       <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
         <li class="dropdown-header">
-          <h6>Syafiqa Zahroo</h6>
+          <h6>Daniel Lim</h6>
           <span>Web Designer</span>
         </li>
         <li>
